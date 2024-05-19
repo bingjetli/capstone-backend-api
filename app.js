7612eeae5d491ad96e45229222d3f0d09456c1bc
Bailey Liang <bingjetli88@gmail.com>
2024-05-19 16:02:23 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//Load the .env file
+dotenv.config();
+
+//Setup and initialize the express application.
+const app = express();
+const port = process.env.PORT || '8888';
+
+//Configure JSON handling middleware.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Configure express to allow cross-origin requests.
+app.use(function (req, res, next) {
+    res.header(
+        'Access-Control-Allow-Origin',
+        process.env.CROSS_ORIGIN_ALLOWED_URL
+    ); // Set to your frontend origin
+    // res.header("Access-Control-Allow-Origin", "*"); // Allow all origins (not recommended)
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
+//Setup the database context.
+const db = require('./database_context.js');
+db.initialize(process.env.DB_URL, process.env.DB_NAME);
+
+//Setup API routing.
+app.get('/', (request, response) => {
+    response.json({
+        content: 'Hello!',
+    });
+});
+
+app.get('/api/projects/get-all', async (request, response) => {
+    const projects = await db.getProjects();
+
+    response.json({
+        content: projects,
+    });
+});
+
+app.get('/api/skills/get-all', async (request, response) => {
+    const projects = await db.getSkills();
+
+    response.json({
+        content: projects,
+    });
+});
+
+//Start the server.
+app.listen(port, () => {
+    console.log(`Server is now listening on port ${port}.`);
+});
---
+++ database_context.js
@@ -0,0 +1,35 @@
+module.exports = {
+    _db: null,
+    _client: null,
+
+    _projects: null,
+    _skills: null,
+    _projects_collection_name: null,
+    _skills_collection_name: null,
+
+    initialize: function (database_url, database_name) {
+        //const {MongoClient, ObjectId}
+        const { MongoClient } = require('mongodb');
+
+        this._client = new MongoClient(database_url);
+        this._db = this._client.db(database_name);
+
+        this._projects_collection_name = 'portfolio-projects';
+        this._projects = this._db.collection(this._projects_collection_name);
+
+        this._skills_collection_name = 'portfolio-skills';
+        this._skills = this._db.collection(this._skills_collection_name);
+    },
+
+    getProjects: async function () {
+        const results = this._projects.find({});
+
+        return await results.toArray();
+    },
+
+    getSkills: async function () {
+        const results = this._skills.find({});
+
+        return await results.toArray();
+    },
+};
